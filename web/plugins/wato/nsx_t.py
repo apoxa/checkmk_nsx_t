0adc07732b6687f2ccab3272398ed68a55da5135
Benjamin Stier <b.stier@levigo.de>
2021-01-27 12:18:57 +0100

    removed cruft from nsx_backups rewrite

--- web/plugins/wato/nsx_t.py
+++ web/plugins/wato/nsx_t.py
@@ -19,38 +19,6 @@
 
 
 register_check_parameters(
-    subgroup_applications,
-    "backups",
-    _("Time since last backup"),
-    Dictionary(
-        elements=[
-            (
-                "levels",
-                Tuple(
-                    title=_("Time since of last backup"),
-                    elements=[
-                        Integer(
-                            title=_("Warning Level for time since last backup"),
-                            help=_("Warning Level for time since last backup."),
-                            unit=_("days"),
-                            default_value=7,
-                        ),
-                        Integer(
-                            title=_("Critical Level for time since last backup"),
-                            help=_("Critical Level for time since last backup."),
-                            unit=_("days"),
-                            default_value=14,
-                        ),
-                    ],
-                ),
-            ),
-        ],
-    ),
-    None,
-    "dict",
-)
-
-register_check_parameters(
     subgroup_os,
     "nsx_memory",
     _("Main memory usage of VMWare NSX system"),
