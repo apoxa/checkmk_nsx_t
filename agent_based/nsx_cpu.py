f187b497fd96330f8ec710e215dd37eaa2cde188
Benjamin Stier <b.stier@levigo.de>
2021-01-27 14:51:36 +0100

    use existing cpu-checks for nsx_cpu

--- agent_based/nsx_cpu.py
+++ agent_based/nsx_cpu.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import Dict, Union, List, Mapping, Any
+from typing import Dict, Union, List
 
-from .agent_based_api.v1 import register, Service, State, Result
-from .agent_based_api.v1.type_defs import StringTable, CheckResult, DiscoveryResult
+from .agent_based_api.v1 import register
+from .agent_based_api.v1.type_defs import StringTable
 
-from cmk.base.check_legacy_includes.cpu_load import check_cpu_load_generic
-
-Section = Dict[str, Union[float, List[float]]]
+Section = Dict[str, Union[float, List[float], int]]
 
 
 def parse_nsx_cpu(string_table: StringTable) -> Section:
     cpu_info: Section = {
         "num_cpus": int(string_table[0][0]),
         "load": [float(i) for i in string_table[1]],
+        "num_threads": 0,  # this is actually wrong, but I don't know how to exclude the cpu_threads check
     }
 
     return cpu_info
@@ -23,27 +22,5 @@ def parse_nsx_cpu(string_table: StringTable) -> Section:
 register.agent_section(
     name="nsx_cpu",
     parse_function=parse_nsx_cpu,
-)
-
-
-def discover_nsx_cpu(section: Section) -> DiscoveryResult:
-    if not section:
-        return
-    yield Service()
-
-
-def check_nsx_cpu(params: Mapping[str, Any], section: Section) -> CheckResult:
-    state, summary, perfdata = check_cpu_load_generic(
-        {}, section["load"], section["num_cpus"]
-    )
-    yield Result(state=State(state), summary=summary)
-    # TODO show metrics
-
-
-register.check_plugin(
-    name="nsx_cpu",
-    service_name="CPU utilization",
-    discovery_function=discover_nsx_cpu,
-    check_function=check_nsx_cpu,
-    check_default_parameters={},
+    parsed_section_name="cpu",
 )
